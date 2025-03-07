b549efc1b9efdc766811873a4c3473ab50c0686b
Darkdevolt <diopibrahimabdallah@gmail.com>
2025-03-07 01:38:54 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,22 @@
+import streamlit as st
 import data_manager
 
 def main():
-    # Chemin du fichier CSV
-    file_path = "HistoricalPrices.csv"
+    st.title("Gestion des données CSV")
     
-    # Traiter les données
-    df = data_manager.process_data(file_path)
+    # Téléverser un fichier CSV
+    uploaded_file = st.file_uploader("Téléversez un fichier CSV", type=["csv"])
     
-    if df is not None:
-        print("Données traitées avec succès !")
-        print(df.head())  # Afficher les premières lignes du DataFrame traité
-    else:
-        print("Le traitement des données a échoué.")
+    if uploaded_file is not None:
+        # Traiter les données
+        df = data_manager.process_data(uploaded_file)
+        
+        if df is not None:
+            st.success("Données traitées avec succès !")
+            st.write("Aperçu des données :")
+            st.dataframe(df)  # Afficher le DataFrame traité
+        else:
+            st.error("Le traitement des données a échoué. Vérifiez la structure du fichier CSV.")
 
 if __name__ == "__main__":
     main()
